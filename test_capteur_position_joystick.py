b31f782c80cca0ebd0f90077c921c29b7f085efd
Christian Ducros <96232175+christianDUCROS@users.noreply.github.com>
2023-07-03 11:15:56 +0200

    Update test_capteur_position_joystick.py

--- test_capteur_position_joystick.py
+++ test_capteur_position_joystick.py
@@ -1,6 +1,7 @@
 # Code adapté de :
 # https://github.com/itechnofrance/micropython/blob/master/librairies/paj7620/use_paj7620.py
-# Utilisation de librairie du capteur de gestes PAJ7620
+# Utilisation de librairie du capteur de gestes PAJ7620 adaptée disponible sous 
+# https://github.com/christianDUCROS/capteur_paj7620/edit/main 
 from machine import Pin, SoftI2C
 import time
 import paj7620
@@ -10,9 +11,6 @@ i2c = SoftI2C(scl=Pin(5), sda=Pin(4), freq=400000)
 
 #instanciation capoteur de gestee
 g= paj7620.PAJ7620(i2c)
-
-# Liste des gestes
-gestures = [ "Aucun geste", "Avant", "Arrière", "Droite", "Gauche", "Haut", "Bas", "Sens horaire", "Sens anti-horaire", "Vague"]
 
 while True:
     # Lecture position joystick
